9305b8b08694aac564f778a4f257af55f32fb258
Matthew General <mgeneral@synacor.com>
2015-09-25 16:33:25 -0400

    Running VM!

--- vm.py
+++ vm.py
@@ -1,13 +1,17 @@
 from os import *
 import sys
 import struct
+import logging
 
+logging.basicConfig(filename="instructions.log", level=logging.DEBUG)
+logger = logging.getLogger('instructions')
+logger.addHandler(logging.handlers.MemoryHandler())
 '''The vm'''
 class Vm(object):
     MAX_VALUE = 32768
     '''Vm takes a file_name to read and start executing'''
     def __init__(self, file_name):
-        self.address = -1;
+        self.address = 0;
         self.stack = []
         self.registers = {
                 0: 0,
@@ -21,7 +25,15 @@ class Vm(object):
                 }
         self.memory = {}
 
-        self.fd = open(file_name, "rb")
+        fd = open(file_name, "rb")
+        word = fd.read(2)
+        i = 0
+        while word:
+            data = struct.unpack("H", word)[0]
+            self.memory[i] = data
+            word = fd.read(2)
+            i = i + 1
+        logging.debug("loaded up %s 16bit items" % i)
 
     '''start begins reading the file'''
     def start(self):
@@ -32,9 +44,8 @@ class Vm(object):
 
     '''read reads 16 bits (2 bytes) at a time and returns the data'''
     def read(self, raw=False):
+        value = self.memory[self.address]
         self.address = self.address + 1
-        data = self.fd.read(2)
-        value = struct.unpack('H', data)[0]
         if raw:
             return value
         else:
@@ -70,62 +81,49 @@ class Vm(object):
         try:
             dispatch.get(int(data))()
         except TypeError as e:
-            print("Tried executing %s" % data)
-            print(e)
+            logging.debug("Tried executing %s" % data)
+            logging.debug(e)
             exit()
 
     ''' jumps to address number, address is converted to the byte number by multiplying by 2 since values are 16bit'''
     def jump(self, address):
-        print("Old address %s new address %s" % (self.address, address))
-        self.fd.seek(address * 2)
         self.address = address
 
     ''' if returns value of reg if looking for register'''
     def deref(self, value):
         if value >= self.MAX_VALUE:
-            print("Dereferencing %s" % value)
             return self.get_reg(value)
         return value
 
     '''returns the value in a register for a given value'''
     def get_reg(self, reg):
         reg = reg - self.MAX_VALUE
-        print("register %s" % reg)
         return self.registers.get(reg)
 
     '''sets the register to the value'''
     def set_reg(self, reg, value):
         reg = reg - self.MAX_VALUE
-        print("setting register %s to %s" % (reg, value))
         self.registers[reg] = value
-
-    '''stack_pop pops a value off the stack'''
-    def stack_pop(self):
-        return self.stack.pop()
-
-    '''stack_push pushes a value onto the stack'''
-    def stack_push(self, value):
-        self.stack.append(value)
 
     '''set register <a> to the value of <b>'''
     def set(self):
         a = self.read(raw=True)
         b = self.read()
-        print("set %s %s" % (a, b))
+        logging.debug("set %s %s" % (a, b))
         self.set_reg(a, b)
 
     '''push <a> onto the stack'''
     def push(self):
         a = self.read()
-        self.stack_push(a);
-        print("push %s" % a)
+        self.stack.append(a);
+        logging.debug("push %s" % a)
 
     '''remove the top element from the stack and write it into <a>; empty stack = error'''
     def pop(self):
         a = self.read(raw=True)
-        value = self.stack_pop();
+        value = self.stack.pop();
         self.set_reg(a, value)
-        print("pop %s" % a)
+        logging.debug("pop %s" % a)
 
     '''set <a> to 1 if <b> is equal to <c>; set it to 0 otherwise'''
     def eq(self):
@@ -134,7 +132,7 @@ class Vm(object):
         c = self.read()
         eq = b == c
         self.set_reg(a, int(eq))
-        print("eq %s %s %s" % (a, b, c))
+        logging.debug("eq %s %s %s" % (a, b, c))
 
     '''set <a> to 1 if <b> is greater than <c>; set it to 0 otherwise'''
     def gt(self):
@@ -142,7 +140,7 @@ class Vm(object):
         b = self.read()
         c = self.read()
         self.set_reg(a, int(b > c))
-        print("gt %s %s %s" % (a, b, c))
+        logging.debug("gt %s %s %s" % (a, b, c))
 
     '''jump to <a>'''
     def jmp(self):
@@ -153,7 +151,7 @@ class Vm(object):
     def jt(self):
         a = self.read()
         b = self.read()
-        print("jt %s %s" % (a, b))
+        logging.debug("jt %s %s" % (a, b))
         if a != 0:
             self.jump(b)
 
@@ -161,7 +159,7 @@ class Vm(object):
     def jf(self):
         a = self.read()
         b = self.read()
-        print("jf %s %s" % (a, b))
+        logging.debug("jf %s %s" % (a, b))
         if a == 0:
             self.jump(b)
 
@@ -170,23 +168,28 @@ class Vm(object):
         a = self.read(raw=True)
         b = self.read()
         c = self.read()
-        sum = b + c % self.MAX_VALUE
+        sum = (b + c) % self.MAX_VALUE
         self.set_reg(a, sum)
-        print("add %s %s %s" % (a, b, c))
+        logging.debug("add %s %s %s" % (a, b, c))
 
     '''store into <a> the product of <b> and <c> (modulo 32768)   '''
     def mult(self):
-        a = self.read()
+        a = self.read(raw=True)
         b = self.read()
         c = self.read()
-        print("mult %s, %s, %s" % (a, b, c))
+        product = (b * c) % self.MAX_VALUE
+        self.set_reg(a, product)
+        logging.debug("mult %s, %s, %s" % (a, b, c))
+        logging.debug("product %s" % product)
 
     '''store into <a> the remainder of <b> divided by <c>'''
     def mod(self):
-        a = self.read()
+        a = self.read(raw=True)
         b = self.read()
         c = self.read()
-        print("mod %s %s %s" % (a, b, c))
+        mod = b % c
+        self.set_reg(a, mod)
+        logging.debug("mod %s %s %s" % (a, b, c))
 
     '''stores into <a> the bitwise and of <b> and <c>'''
     def and_fn(self):
@@ -194,7 +197,7 @@ class Vm(object):
         b = self.read()
         c = self.read()
         self.set_reg(a, (b & c) % self.MAX_VALUE)
-        print("and %s %s %s" % (a, b, c))
+        logging.debug("and %s %s %s" % (a, b, c))
 
     '''stores into <a> the bitwise or of <b> and <c>'''
     def or_fn(self):
@@ -202,7 +205,7 @@ class Vm(object):
         b = self.read()
         c = self.read()
         self.set_reg(a, (b | c) % self.MAX_VALUE)
-        print("or %s %s %s" % (a, b, c))
+        logging.debug("or %s %s %s" % (a, b, c))
 
     '''stores 15-bit bitwise inverse of <b> in <a>'''
     def not_fn(self):
@@ -211,35 +214,39 @@ class Vm(object):
 
         not_val = (~b & 0xFFFF) % self.MAX_VALUE
         self.set_reg(a, not_val)
-        print("not %s %s" % (a, b))
+        logging.debug("not %s %s" % (a, b))
 
     '''read memory at address <b> and write it to <a>'''
     def rmem(self):
-        a = self.read()
+        a = self.read(raw=True)
         b = self.read()
-        print("rmem %s %s" % (a, b))
+        self.set_reg(a, self.memory[b])
+
+        logging.debug("rmem %s %s" % (a, b))
 
     '''write the value from <b> into memory at address <a>'''
     def wmem(self):
         a = self.read()
         b = self.read()
-        print("wmem %s %s" % (a, b))
+        self.memory[a] = b
+        logging.debug("wmem %s %s" % (a, b))
 
     '''write the address of the next instruction to the stack and jump to <a>'''
     def call(self):
         a = self.read()
-        self.stack_push(self.address + 1)
+        logging.debug("Calling %s at %s, next instruction at %s" % (a, self.address-1, self.address))
+        self.stack.append(self.address)
 
         self.jump(a)
-        print("call %s" % a)
+        logging.debug("call %s" % a)
 
     '''remove the top element from the stack and jump to it; empty stack = halt'''
     def ret(self):
         if len(self.stack) == 0:
             exit()
-        el = self.stack_pop()
+        el = self.stack.pop()
         self.jump(el)
-        print("ret")
+        logging.debug("ret")
 
     '''write the character represented by ascii code <a> to the terminal'''
     def out_fn(self):
@@ -249,12 +256,14 @@ class Vm(object):
 
     '''read a character from the terminal and write its ascii code to <a>; it can be assumed that once input starts, it will continue until a newline is encountered; this means that you can safely read whole lines from the keyboard and trust that they will be fully read'''
     def in_fn(self):
-        a = self.read()
-        print("in %s" % a);
+        a = self.read(raw=True)
+        user_input = sys.stdin.read(1)
+        self.set_reg(a, ord(user_input))
+        logging.debug("in %s" % a);
 
     '''no operation'''
     def noop(self):
-        print("noop")
+        logging.debug("noop")
         pass
 
 vm = Vm("challenge.bin")
